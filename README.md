c3c677b1b725c014b389180c5bcbc1066d4a406a
jaychouzzz <f18326186224@gmail.com>
2023-07-29 14:08:16 +0800

    ✨ feat: 完成

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # domestic-rules-generator
 
 ## 设计初衷
-* 由于xray客户端使用白名单模式(禁广告,bt直连,国内域名走直连,其他全部走代理)
-* 服务端屏蔽了国内ip,需要手动在xray-parser项目的路由规则手动添加国内域名,比较繁琐
-* 配合github actions可以实现一劳永逸的在xray-parser项目自动添加国内域名路由规则
+
+- 由于 xray 客户端使用白名单模式(禁广告,bt 直连,国内域名走直连,其他全部走代理)
+- 服务端屏蔽了国内 ip,需要手动在 xray-parser 项目的路由规则手动添加国内域名,比较繁琐
+- 配合 github actions 可以实现一劳永逸的在 xray-parser 项目自动添加国内域名路由规则
 
 ## 国内域名规则生成器
-* 根据xray服务端的日志文件 分析出[block]的规则集
-* 将规则集去重添加到现有的xray-parser项目(client分支)的路由配置文件中
+
+- 根据 xray 服务端的日志文件 分析出[block]的规则集
+- 将规则集去重添加到现有的 xray-parser 项目(client 分支)的路由配置文件中
+
+## 操作步骤
+
+- 从服务器下载/var/log/xray/access.log 文件,放到 datasource 目录下
+- 运行 python3 main.py
+- 将生成的 routing_body.json 文件拷贝到 xray-parser 项目的 client 分支的 routing 目录下,提交更新
+- 客户端更新配置文件
--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 #!/usr/local/bin/python3.8
 # -*- coding: utf-8 -*-
+import json
+import random
+import string
 
+res_dict = {}
 
+# 过滤出/datasource/access.log文件中包含[block]的行，并将结果写入到/block.log文件中
+def filter_block_to_file():
+    with open('datasource/access.log', 'r+') as f:
+            for line in f:
+                if '[block]' in line:
+                    # 获取accepted和[block]之间的域名
+                    domain = line.split('accepted')[1].split('[block]')[0].strip().split(':')[1]
+                    # 如果domain不是ip address
+                    if not domain.replace('.', '').isdigit():
+                        res_dict[domain] =  line
+    
+    # 将res_dict的keys写入dist/block.log文件中
+    with open('dist/block.log', 'w+') as f:
+        for key in res_dict.keys():
+            f.write(key + '\n')              
+            
+    # 将res_dist的keys写入routing_body.json的rules中
+    with open('routing_template_body.json', 'r+') as f:
+        str_json = json.loads(f.read())
+        for key in res_dict.keys():
+            # {
+            #     "id": "qO1yH8rK5nG6qQ0f",
+            #     "type": "field",
+            #     "outboundTag": "direct",
+            #     "domain": [
+            #         "codemart.com"
+            #     ]
+            # }
+            # 生成16位随机字符串
+            id = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+            str_json['rules'].append({
+                'id': id,
+                'type': 'field',
+                'outboundTag': 'direct',
+                'domain': [key]
+            })
+        # 将str_json写入到dist/routing_body.json文件中
+        with open('dist/routing_body.json', 'w+') as f:
+            f.write(json.dumps(str_json, indent=4))
+            
+                    
+if __name__ == '__main__':
+    filter_block_to_file()                    
