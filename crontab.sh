49384f8705299587b79beb2aef70dbf8fe352d46
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-08-24 13:14:15 +0800

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -3,7 +3,5 @@
 cd /root/code/domestic-rules-generator && git pull
 cd /root/code/xray-parser && git checkout client && git pull
 python3.8 /root/code/domestic-rules-generator/main.py
-git config --global user.email "f18326186224@gmail.com"
-git config --global user.name "jayncfzzz"
 cd /root/code/xray-parser && git add . && git commit -m "更新客户端block规则" && git push
 cd /root/code/domestic-rules-generator && git add . && git commit -m "更新增量文件" && git push
